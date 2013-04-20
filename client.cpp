34acb3f6d51ff87036f377bacfb8ec098b10d0bc
Friedrich Schöller <hans@schoeller.se>
2013-04-20 04:06:05 +0200

    Changed email address

--- client.cpp
+++ client.cpp
@@ -1,6 +1,6 @@
 /*
  *  Hans - IP over ICMP
- *  Copyright (C) 2009 Friedrich Schöller <friedrich.schoeller@gmail.com>
+ *  Copyright (C) 2009 Friedrich Schöller <hans@schoeller.se>
  *  
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
