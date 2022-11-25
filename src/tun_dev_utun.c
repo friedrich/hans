f4a544b2d78ceb1d999b260cc275191eb1abca20
Friedrich Schöller <hans@schoeller.se>
2022-11-25 16:59:58 +0100

    clean-up indentation

--- src/tun_dev_utun.c
+++ src/tun_dev_utun.c
@@ -15,68 +15,68 @@
 #include <net/if_utun.h>
 #include <netinet/ip.h>
 
-/* 
- * Allocate TUN device, returns opened fd. 
+/*
+ * Allocate TUN device, returns opened fd.
  * Stores dev name in the first arg(must be large enough).
  */
 int tun_open(char *ifname)
 {
-	struct sockaddr_ctl addr;
-	struct ctl_info info;
-	socklen_t ifname_len = 10;
-	int fd = -1;
-	int err = 0;
-	int unit = 0;
+    struct sockaddr_ctl addr;
+    struct ctl_info info;
+    socklen_t ifname_len = 10;
+    int fd = -1;
+    int err = 0;
+    int unit = 0;
 
-	fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
-	if (fd < 0) {
-		return -1;
-	}
+    fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
+    if (fd < 0) {
+        return -1;
+    }
 
-	/* Look up the kernel controller ID for utun devices. */
-	memset(&info, 0, sizeof(info));
-	strncpy(info.ctl_name, UTUN_CONTROL_NAME, MAX_KCTL_NAME);
+    /* Look up the kernel controller ID for utun devices. */
+    memset(&info, 0, sizeof(info));
+    strncpy(info.ctl_name, UTUN_CONTROL_NAME, MAX_KCTL_NAME);
 
-	err = ioctl(fd, CTLIOCGINFO, &info);
-	if (err != 0) {
-		close(fd);
-		return -1;
-	}
+    err = ioctl(fd, CTLIOCGINFO, &info);
+    if (err != 0) {
+        close(fd);
+        return -1;
+    }
 
-	/* Connecting to the socket creates the utun device. */
-	addr.sc_len = sizeof(addr);
-	addr.sc_family = AF_SYSTEM;
-	addr.ss_sysaddr = AF_SYS_CONTROL;
-	addr.sc_id = info.ctl_id;
+    /* Connecting to the socket creates the utun device. */
+    addr.sc_len = sizeof(addr);
+    addr.sc_family = AF_SYSTEM;
+    addr.ss_sysaddr = AF_SYS_CONTROL;
+    addr.sc_id = info.ctl_id;
 
-	/* Look for a first available utun device */
-	for (unit = 1; unit < 50; unit++) {
-		addr.sc_unit = unit;
-		err = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
-		if (err == 0) {
-			break;
-		}
-	}
-	if (err != 0) {
-		close(fd);
-		return -1;
-	}
+    /* Look for a first available utun device */
+    for (unit = 1; unit < 50; unit++) {
+        addr.sc_unit = unit;
+        err = connect(fd, (struct sockaddr *) &addr, sizeof(addr));
+        if (err == 0) {
+            break;
+        }
+    }
+    if (err != 0) {
+        close(fd);
+        return -1;
+    }
 
-	/* Retrieve the assigned interface name. */
-	err = getsockopt(fd, SYSPROTO_CONTROL, UTUN_OPT_IFNAME, ifname, &ifname_len);
-	if (err != 0) {
-		close(fd);
-		return -1;
-	}
+    /* Retrieve the assigned interface name. */
+    err = getsockopt(fd, SYSPROTO_CONTROL, UTUN_OPT_IFNAME, ifname, &ifname_len);
+    if (err != 0) {
+        close(fd);
+        return -1;
+    }
 
-	/* Set FD_CLOEXEC flag on file descriptor.
-	 * This stops it from being inherited by system() calls.
-	 */
-	if (fcntl(fd, F_SETFD, fcntl(fd, F_GETFD) | FD_CLOEXEC) < 0) {
-		close(fd);
-		return -1;
-	}
-	return fd;
+    /* Set FD_CLOEXEC flag on file descriptor.
+     * This stops it from being inherited by system() calls.
+     */
+    if (fcntl(fd, F_SETFD, fcntl(fd, F_GETFD) | FD_CLOEXEC) < 0) {
+        close(fd);
+        return -1;
+    }
+    return fd;
 }
 
 int tun_close(int fd, char *dev)
@@ -89,16 +89,16 @@ int tun_write(int fd, char *buf, int len)
 {
     u_int32_t type = htonl(AF_INET);
     struct iovec iv[2];
-	int wlen;
-    
+    int wlen;
+
     iv[0].iov_base = &type;
     iv[0].iov_len = sizeof(type);
     iv[1].iov_base = buf;
     iv[1].iov_len = len;
 
-	if ( (wlen = writev(fd, iv, 2)) > 0 )
-		return wlen - sizeof(type);
-	return wlen;
+    if ((wlen = writev(fd, iv, 2)) > 0)
+        return wlen - sizeof(type);
+    return wlen;
 }
 
 int tun_read(int fd, char *buf, int len)
@@ -112,10 +112,10 @@ int tun_read(int fd, char *buf, int len)
     iv[1].iov_base = buf;
     iv[1].iov_len = len;
 
-    if( (rlen = readv(fd, iv, 2)) > 0 )
-       return rlen - sizeof(type);
+    if ((rlen = readv(fd, iv, 2)) > 0)
+        return rlen - sizeof(type);
     else
-       return rlen;
+        return rlen;
 }
 
 const char *tun_last_error()
